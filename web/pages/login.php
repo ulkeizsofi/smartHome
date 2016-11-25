0e0a774d52092446aa23c3e6acfb2a5887105afc
Ulkei Zsofia <ulkeizsofi@gmail.com>
2016-11-26 00:38:38 +0200

    rearrange login labels

--- web/pages/login.php
+++ web/pages/login.php
@@ -7,19 +7,19 @@
     <h2>Login</h2>
     <form class="form-horizontal">
       <div class="form-group">
-        <label class="control-label col-sm-2" for="name">Name:</label>
-        <div class="col-sm-10">
+        <label class="control-label col-sm-3" for="name">Name:</label>
+        <div class="col-sm-8">
           <input type="name" class="form-control" id="name" placeholder="Enter name" name="name">
         </div>
       </div>
       <div class="form-group">
-        <label class="control-label col-sm-2" for="pwd">Password:</label>
-        <div class="col-sm-10">
+        <label class="control-label col-sm-3" for="pwd">Password:</label>
+        <div class="col-sm-8">
           <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="password">
         </div>
       </div>
       <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
+        <div class="col-sm-offset-3 col-sm-8">
           <div class="checkbox">
             <label>
               <input type="checkbox" name="remember"> Remember me
@@ -28,7 +28,7 @@
         </div>
       </div>
       <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
+        <div class="col-sm-offset-3 col-sm-8">
           <button id="login_btn" type="submit" class="btn btn-default">Submit</button>
         </div>
       </div>
@@ -40,31 +40,31 @@
     <h2>Sign up</h2>
     <form class="form-horizontal">
       <div class="form-group">
-        <label class="control-label col-sm-2" for="name">Name:</label>
-        <div class="col-sm-10">
+        <label class="control-label col-sm-3" for="name">Name:</label>
+        <div class="col-sm-8">
           <input type="name" class="form-control" id="name" placeholder="Enter your name" name="name">
         </div>
       </div>
       <div class="form-group">
-        <label class="control-label col-sm-2" for="email">Email:</label>
-        <div class="col-sm-10">
+        <label class="control-label col-sm-3" for="email">Email:</label>
+        <div class="col-sm-8">
           <input type="email" class="form-control" id="email" placeholder="Enter your email address" name="email">
         </div>
       </div>
       <div class="form-group">
-        <label class="control-label col-sm-2" for="psw">Password:</label>
-        <div class="col-sm-10">
+        <label class="control-label col-sm-3" for="psw">Password:</label>
+        <div class="col-sm-8">
           <input type="password" class="form-control" id="pwd" placeholder="Enter your password" name="password">
         </div>
       </div>
       <div class="form-group">
-        <label class="control-label col-sm-2" for="con_psw">Confirm password:</label>
-        <div class="col-sm-10">
+        <label class="control-label col-sm-3" for="con_psw">Confirm password:</label>
+        <div class="col-sm-8">
           <input type="password" class="form-control" id="con_psw" placeholder="Reenter your password" name="con_psw">
         </div>
       </div>
       <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
+        <div class="col-sm-offset-3 col-sm-8">
           <button id="signup_btn" type="signup" class="btn btn-default">Sign up</button>
         </div>
       </div>
